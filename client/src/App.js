21801795f615df11164af911fa01885e4321e855
Harry <nath23927@gmail.com>
2023-06-11 02:08:36 +0530

    user dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import Dashboard from './components/Dashboard'
 import Details from './components/Details';
 import Navbar from './components/Navbar'
+import UserDashboard from './components/UserDashboard';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserForm from './components/UserForm';
+
 
 function App() {
   return (
     <Router>
     <div>
-      <Navbar/>
       <Routes>
-      <Route exact path="/Dashboard" element={<Dashboard />} />
-      <Route path="/Details" element={<Details/>} />
+      <Route exact path="/Dashboard" element={
+      <>
+      <Navbar/>
+      <Dashboard />
+      </>
+      } />
+      <Route exact path="/Details" element={<Details/>} />
+      <Route exact path="/userDashboard" element={<UserDashboard/>} />
+      <Route exact path="/UserForm" element={<UserForm/>} />
 
       </Routes>
 
