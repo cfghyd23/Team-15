aecdd7348d6e8877637b163f039221084ea9cb6f
sruthi burugu <132471373+SRUTHI-SRIVALLI-BURUGU@users.noreply.github.com>
2023-06-17 17:35:04 +0530

    Add files via upload

---
+++ adminlogin.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './style.css';
+
+class LoginRegistration extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="title-text">
+          <div className="title login">
+            Login Form
+          </div>
+          <div className="title signup">
+            Signup Form
+          </div>
+        </div>
+        <div className="form-container">
+          <div className="slide-controls">
+            <input type="radio" name="slide" id="login" defaultChecked />
+            <input type="radio" name="slide" id="signup" />
+            <label htmlFor="login" className="slide login">Login</label>
+            <label htmlFor="signup" className="slide signup">Signup</label>
+            <div className="slider-tab"></div>
+          </div>
+          <div className="form-inner">
+            <form action="#" className="login">
+              <div className="field">
+                <input type="text" placeholder="Email Address" required />
+              </div>
+              <div className="field">
+                <input type="password" placeholder="Password" required />
+              </div>
+              <div className="pass-link">
+                <a href="#">Forgot password?</a>
+              </div>
+              <div className="field btn">
+                <div className="btn-layer"></div>
+                <input type="submit" value="Login" />
+              </div>
+              <div className="signup-link">
+                Not a member? <a href="">Signup now</a>
+              </div>
+            </form>
+            <form action="#" className="signup">
+              <div className="field">
+                <input type="text" placeholder="Enter Name" required />
+              </div>
+              <div className="field">
+                <input type="text" placeholder="Enter Home name" required />
+              </div>
+              <div className="field">
+                <input type="text" placeholder="Enter your mail-id" required />
+              </div>
+              <div className="field">
+                <input type="password" placeholder="Password" required />
+              </div>
+              <div className="field">
+                <input type="password" placeholder="Confirm password" required />
+              </div>
+              <div className="field btn">
+                <div className="btn-layer"></div>
+                <input type="submit" value="Signup" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginRegistration;
